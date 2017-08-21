0e6a07f861f3e05b29a3b55a00d3919864d971fc
unknown <curtis@checkmategaming.com>
2017-08-21 03:14:27 -0400

    TODO work on front end and fix umg

--- test.js
+++ test.js
@@ -1,70 +1,21 @@
 var scrape = require('./scrape');
 var database = require('./database');
 
+var regions = {};
 
-var user = {
-	_id: '1234',
-	username: 'coolusername',
-	website: 'mlg.stuff',
-	region: 'EU',
-	tournaments: ['t1','t2','t3'],
-	platforms: ['p1'],
-	games: ['g1']
-};
-
-var userU = {
-	_id: '1234',
-	username: 'coolusernamed',
-	website: 'somewebsite',
-	region: 'NA',
-	twitter: 'twitter',
-	tournaments: ['t6'],
-	platforms: ['p4'],
-	games: ['g3']
-}
-
-doStuff = async () => {
-	try {
-		await database.deleteAll();
-		await database.upsertUser(user);
-		var docs = await database.query({}, 'users');
-		console.log(docs);
-		await database.upsertUser(userU);
-		var docs = await database.query({}, 'users');
-		console.log(docs);
-	} catch (err) {
-		console.log(err);
+database.query('tournaments', { "website": "gamebattles.majorleaguegaming.com", "updated": { "$lt": new Date(2017, 7, 19, 7) } }).then(docs => {
+	for(doc of docs){
+		if(regions[doc.region] == undefined){
+			regions[doc.region] = {};
+		}
+		if(regions[doc.region][doc.platform] == undefined){
+			regions[doc.region][doc.platform] = {};
+		}
+		if(regions[doc.region][doc.platform][doc.game] == undefined){
+			regions[doc.region][doc.platform][doc.game] = 0;
+		} else {
+			regions[doc.region][doc.platform][doc.game] += doc.users.length;
+		}
 	}
-	await database.close();
-};
-
-//doStuff();
-
-var gb = require('./gb');
-var umg = require('./umg');
-
-/*
-gb.checkTournaments().then(tournaments => {
-	console.log(tournaments);
+	console.log(regions);
 });
-*/
-
-
-var profile = 'http://profile.majorleaguegaming.com/-OaKLeY-';
-var url = 'https://umggaming.com/tournaments/2250';
-
-umg.processTournament(url).then(tournament => {
-	console.log(tournament);
-});
-
-/*
-gb.processUser(profile, 'tournament', 'region', 'game').then(user => {
-	console.log(user);
-});
-*/
-
-/*
-scrape(profile).then(html => {
-	console.log(html);
-});
-*/
